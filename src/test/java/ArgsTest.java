f28ee2ff7c196cfa58941288f098b7d131ce6224
yixiaoqi <916510303@qq.com>
2023-12-12 23:07:33 +0800

    完成两个API构想的测试。

--- src/test/java/ArgsTest.java
+++ src/test/java/ArgsTest.java
@@ -1,18 +1,28 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArgsTest {
     @Test
-    void should() {
+    void should_example_1() {
         Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr");
 
-        assertEquals(options.logging(), true);
-        assertEquals(options.port(), 8080);
-        assertEquals(options.dir(), "/usr");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr", options.dir());
     }
 
     record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String dir) {
     }
 
+    @Test
+    void should_example_2() {
+        ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
+
+        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.strings());
+        assertArrayEquals(new int[]{1, 2, -3, 5}, options.ints());
+    }
+
+    record ListOptions(@Option("g") String[] strings, @Option("d") int[] ints) {
+    }
 }
