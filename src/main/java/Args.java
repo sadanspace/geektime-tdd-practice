734e26814329857ca81f363940e4b530a380d51d
yixiaoqi <916510303@qq.com>
2023-12-30 22:15:00 +0800

    重构：提取接口，完成实现类和函数替换。

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -20,16 +20,56 @@ public class Args {
         Object value = null;
         Option option = parameter.getAnnotation(Option.class);
         if (parameter.getType() == boolean.class) {
-            value = arguments.contains("-" + option.value());
+            value = parseBoolean(arguments, option);
         }
         if (parameter.getType() == int.class) {
-            int index = arguments.indexOf("-" + option.value());
-            value = Integer.parseInt(arguments.get(index + 1));
+            value = parseInt(arguments, option);
         }
         if (parameter.getType() == String.class) {
-            int index = arguments.indexOf("-" + option.value());
-            value = arguments.get(index + 1);
+            value = parseString(arguments, option);
         }
         return value;
     }
+
+    interface OptionParser {
+        Object parse(List<String> arguments, Option option);
+    }
+
+    private static Object parseString(List<String> arguments, Option option) {
+        return new StringOptionParser().parse(arguments, option);
+    }
+
+    static public class StringOptionParser implements OptionParser {
+
+        @Override
+        public Object parse(List<String> arguments, Option option) {
+            int index = arguments.indexOf("-" + option.value());
+            return arguments.get(index + 1);
+        }
+    }
+
+    private static Object parseInt(List<String> arguments, Option option) {
+        return new IntOptionParser().parse(arguments, option);
+    }
+
+    private static Object parseBoolean(List<String> arguments, Option option) {
+        return new BooleanOptionParser().parse(arguments, option);
+    }
+
+    static public class IntOptionParser implements OptionParser {
+
+        @Override
+        public Object parse(List<String> arguments, Option option) {
+            int index = arguments.indexOf("-" + option.value());
+            return Integer.parseInt(arguments.get(index + 1));
+        }
+    }
+
+    static public class BooleanOptionParser implements OptionParser {
+        @Override
+        public Object parse(List<String> arguments, Option option) {
+            return arguments.contains("-" + option.value());
+        }
+    }
+
 }
